9db8077eb3d5fd023631074caa4d234367e2607e
webiovg <kevin.kowalsky1@gmail.com>
2022-05-20 10:29:29 +0200

    html and css done, js @fun begins

---
+++ script.js
@@ -0,0 +1,14 @@
+// Declare variables
+let notesKey = ['keyQ', 'keyS', 'keyD', 'keyF', 'keyG', 'keyH', 'keyJ', 'keyK', 'keyL'];
+
+// Get HTML elements
+let notes = document.getElementsByClassName('select');
+
+// Main
+window.addEventListener('keydown', function(event) {
+    console.log(event.code);
+
+    if (event.code = 'keyQ') {
+        
+    }
+})

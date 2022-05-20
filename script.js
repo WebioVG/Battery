20ccbd5795a2635f0596926eacfddfe083efb47c
webiovg <kevin.kowalsky1@gmail.com>
2022-05-20 11:12:16 +0200

    keyboard plays audio

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 // Declare variables
-let notesKey = ['keyQ', 'keyS', 'keyD', 'keyF', 'keyG', 'keyH', 'keyJ', 'keyK', 'keyL'];
+let notesKey = ['KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL'];
+let audios = [new Audio('sounds/clap.wav'), new Audio('sounds/hihat.wav'), new Audio('sounds/kick.wav'), new Audio('sounds/openhat.wav'), new Audio('sounds/boom.wav'), new Audio('sounds/ride.wav'), new Audio('sounds/snare.wav'), new Audio('sounds/tom.wav'), new Audio('sounds/tink.wav')];
 
 // Get HTML elements
 let notes = document.getElementsByClassName('select');
 
 // Main
-window.addEventListener('keydown', function(event) {
-    console.log(event.code);
+for (let note of notes) {
+    console.log(note);
+    note.classList.remove('selected');
+}
 
-    if (event.code = 'keyQ') {
-        
+window.addEventListener('keydown', function(event) {
+    let counter = -1;
+
+    for (let note of notesKey) {
+        counter++;
+
+        if (event.code == note) {
+            audios[counter].play();
+            notes[counter].classList.add('selected');
+        } else {
+            notes[counter].classList.remove('selected');
+        }
     }
 })
